c4267cc56aa0ca759912ba9e2bbf8c093362b2c9
umaru <perrytay666@gmail.com>
2025-02-23 04:12:18 +0800

    Initialize web-store project with frontend and backend separation

---
+++ backend/resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Umaru SunShine</title>
+</head>
+<body>
+    <h1>Welcome to the Umaru Sunshine</h1>
+    <p>Click the button below to view the list of Umaru</p>
+    <a href="" class="btn">Umaru Sunshineeee</a>
+</body>
+</html>
